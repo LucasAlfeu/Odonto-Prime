007faa922c5f5716c0ae7150a7d60c3a5f318146
LucasAlfeu <lucas.a1feu19@gmail.com>
2024-07-26 16:03:35 -0300

    Cadastro de avatar

--- cadastroPaciente.php
+++ cadastroPaciente.php
@@ -1,3 +1,124 @@
+<?php 
+    $nomeResp = $emailResp = $telResp = "";
+    $nomeResErr = $emailRespErr = $telRespErr = "";
+    $nomePaci = $nasciPaci = "";
+    $nomePaciErr = $nasciPaciErr = "";
+    $cabelo = $rosto = $pele = $torso = $pernas = "";
+ 
+    function teste_entrada($data){
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+     }
+ 
+     if($_SERVER["REQUEST_METHOD"] == "POST"){
+        if(empty($_POST["nome_resp"])){
+            $nomeResErr = "Nome do Responsável Obrigatório";
+        } else {
+            $nomeResp = teste_entrada($_POST["nome_resp"]);
+            if(!preg_match("/^[a-zA-Z]*$/", $nomeResp)){
+                $nomeResErr = "Apenas letras e espaços em branco são permitidos";
+            }
+        }
+ 
+        if(empty($_POST["email_resp"])){
+            $emailRespErr = "Email do Responsável Obrigatório";
+        } else {
+            $emailResp = teste_entrada($_POST["email_resp"]);
+            if(!filter_var($emailResp, FILTER_VALIDATE_EMAIL)){
+                $emailRespErr = "Email do responsável no formato errado";
+            }
+        }
+ 
+        if(empty($_POST["tel_resp"])){
+            $telRespErr = "Telefone do Responsável Obrigatório";
+        } else {
+            $telResp = teste_entrada($_POST["tel_resp"]);
+            if(!preg_match("/^[0-9]*$/", $telResp)){
+                $telRespErr = "Apenas números e espaços em branco são permitidos";
+            }
+        }
+ 
+        if(empty($_POST["nome_paci"])){
+            $nomePaciErr = "Nome do Paciente Obrigatório";
+        } else {
+            $nomePaci = teste_entrada($_POST["nome_paci"]);
+            if(!preg_match("/^[a-zA-Z]*$/", $nomePaci)){
+                $nomePaciErr = "Apenas letras e espaços em branco são permitidos";
+            }
+        }
+ 
+        if(empty($_POST["nasci_paci"])){
+            $nasciPaciErr = "Data de Nascimento do Paciente Obrigatório";
+        } else {
+            $nasciPaci = teste_entrada($_POST["nasci_paci"]);
+        } 
+ 
+        $cabeloAux = $_POST["cabelo_lego"];
+        $rostoAux = $_POST["rosto_lego"];
+        $peleAux = $_POST["pele_lego"];
+        $torsoAux = $_POST["tronco_lego"];
+        $pernasAux = $_POST["pernas_lego"];
+ 
+        $cabelo = "./assets/images/cabelo/".$cabeloAux.".png";
+        $rosto = "./assets/images/rosto/".$rostoAux.".png";
+        $pele = "./assets/images/pele/".$peleAux.".png";
+        $troso = "./assets/images/torso/".$torsoAux.".png";
+        $pernas = "./assets/images/pernas/".$pernasAux.".png";
+ 
+        if (empty($nomeResErr) && empty($emailRespErr) && empty($telRespErr) && empty($nomePaciErr) && empty($nasciPaciErr)) {             
+            $servidor = "localhost"; // ou o IP do seu servidor MySQL
+            $usuario = "root"; // seu usuário MySQL
+            $senha = ""; // sua senha MySQL
+            $banco = ""; // o nome do seu banco de dados
+             
+            // Cria uma nova conexão MySQLi
+            $conn = new mysqli($servidor, $usuario, $senha, $banco);
+             
+            // Verifica se a conexão foi bem-sucedida
+            if ($conn->connect_error) {
+                die("Conexão falhou: " . $conn->connect_error);
+                echo "<script type='javascript'>alert('Erro!');";
+                echo "javascript:window.location='../../../cadastroPaciente.php';</script>";
+            }
+             
+            $nameDataBase = "web1";
+            mysqli_select_db($conn, $nameDataBase);
+
+            //Adicionar um responsãvel
+            $sql = "INSERT INTO responsavel(nome_Resp, email_Resp, contato_Resp) VALUES('$nomeResp', '$emailResp', '$telResp')";
+            if ($conn->query($sql) === TRUE) {
+                echo "Novo registro criado com sucesso";
+            } else {
+                echo "Erro: " . $sql . "<br>" . $conn->error;
+            }
+
+            $sql_idResp = "SELECT id_Resp FROM responsavel WHERE nome_Resp='$nomeResp' AND email_Resp='$emailResp' AND contato_Resp='$telResp'" ; 
+            $id_resp = $conn->query($sql);
+
+            //Adicionar um avatar
+            $sql_avatar = "INSERT INTO avatar(pele_Avat, rosto_Avat, cabelo_Avat, torso_Avat, pernas_Avat) VALUES('$pele', '$rosto', '$cabelo', '$torso', '$pernas')";
+            if ($conn->query($sql_avatar) === TRUE) {
+                echo "Novo registro criado com sucesso";
+            } else {
+                echo "Erro: " . $sql . "<br>" . $conn->error;
+            }
+
+            $sql_idAvat = "SELECT id_Avat FROM responsavel WHERE pele_Avat='$pele' AND rosto_Avat='$rosto' AND cabelo_Avat='$cabelo' AND torso_Avat='$torso' AND pernas_Avat='$pernas'" ; 
+            $id_avat = $conn->query($sql);
+
+            //Cadastro De Paciente
+            $sql_paciente = "INSERT INTO paciente(nome_Paci, dataNasci_Pac, id_Resp, id_Avat) VALUES('$nomePaci', '$nasciPaci', '$id_resp', '$id_avat')";
+            if ($conn->query($sql_paciente) === TRUE) {
+                echo "Novo registro criado com sucesso";
+            } else {
+                echo "Erro: " . $sql . "<br>" . $conn->error;
+            }
+        }
+    }
+ ?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -14,7 +135,7 @@
     <main class="principal">
         <a href="" class="princial__volta">Voltar</a>
         <h1 class="principal__title">Cadastro de Paciente</h1>
-        <form action="" class="principal__formulario">
+        <form action="cadastroPaciente.php" class="principal__formulario" method="POST">
             <h2 class="formulario__titulo">Responsável</h2>
             <div class="form__box">
                 Nome: <input type="text" class="form__input" name="nome_resp">
@@ -70,115 +191,28 @@
                     </div>
                 </div>
             </section>
-            <input type="submit" value="Cadastrar" class="form__btnCadastrar" onclick="mostraPaths()">
+            <input type="submit" value="Cadastrar" class="form__btnCadastrar" onclick="">
         </form>
+        <div>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+        </div>
     </main>
+
     <footer class="footer">
         <img class="footer__img" src="./assets/images/footer.png" alt="">
     </footer>
-    
-
-<?php 
-   $nomeResp = $emailResp = $telResp = "";
-   $nomePaci = $nasciPaci = "";
-   $cabelo = $rosto = $pele = $torso = $pernas = "";
-
-   function teste_entrada($data){
-        $data = trim($data);
-        $data = stripslashes($data);
-        $data = htmlspecialchars($data);
-        return $data;
-    }
-
-    if($_SERVER["REQUEST_METHOD"] == "POST"){
-        if(empty($_POST["nome_resp"])){
-            $nomeResp = "Nome do Responsável Obrigatório";
-        } else {
-            $nomeResp = teste_entrada($_POST["nome_resp"]);
-            if(!preg_match("/^[a-zA-Z]*$/", $nomeResp)){
-                $nomeResp = "Apenas letras e espaços em branco são permitidos";
-            }
-        }
-
-        if(empty($_POST["email_resp"])){
-            $emailResp = "Email do Responsável Obrigatório";
-        } else {
-            $emailResp = teste_entrada($_POST["email_resp"]);
-            if(!filter_var($emailResp, FILTER_VALIDATE_EMAIL)){
-                $emailResp = "Email do responsável no formato errado";
-            }
-        }
-
-        if(empty($_POST["tel_resp"])){
-            $telResp = "Telefone do Responsável Obrigatório";
-        } else {
-            $telResp = teste_entrada($_POST["tel_resp"]);
-            if(!preg_match("/^[0-9]*$/", $telResp)){
-                $telResp = "Apenas números e espaços em branco são permitidos";
-            }
-        }
-
-        if(empty($_POST["nome_paci"])){
-            $nomePaci = "Nome do Paciente Obrigatório";
-        } else {
-            $nomePaci = teste_entrada($_POST["nome_paci"]);
-            if(!preg_match("/^[a-zA-Z]*$/", $nomePaci)){
-                $nomePaci = "Apenas letras e espaços em branco são permitidos";
-            }
-        }
-
-        if(empty($_POST["nasci_paci"])){
-            $nasciPaci = "Data de Nascimento do Paciente Obrigatório";
-        } else {
-            $nasciPaci = teste_entrada($_POST["nasci_paci"]);
-        } 
-
-        $cabeloAux = $_POST["cabelo_lego"];
-        $rostoAux = $_POST["rosto_lego"];
-        $peleAux = $_POST["pele_lego"];
-        $torsoAux = $_POST["tronco_lego"];
-        $pernasAux = $_POST["pernas_lego"];
-
-        $cabelo = "./assets/images/cabelo/".$cabeloAux.".png";
-        $rosto = "./assets/images/rosto/".$rostoAux.".png";
-        $pele = "./assets/images/pele/".$peleAux.".png";
-        $troso = "./assets/images/torso/".$torsoAux.".png";
-        $pernas = "./assets/images/pernas/".$pernasAux.".png";
-
-        $url = 'localhost';
-        $usuario = 'root';
-        $senha = '';
-        $dataBase = 'web1';
-        $link = new mysqli($url, $usuario, $senha, $dataBase);
-        $link->set_charset('utf8');
-
-        
-        
-        //$query_resp = "INSERT INTO responsavel(nome_Resp, email_Resp, tel_Resp) VALUES(:nome, :email: tel)";
-        $query_resp ="INSERT INTO responsavel(nome_Resp, email_Resp, tel_Resp) VALUES(?,?,?)";
-        $stmt = $link->prepare($query_resp);
-        $stmt->bind_param("sss", $nomeResp, $emailResp, $telResp);
-
-        if ($stmt->execute()) {
-            echo "Dados inseridos com sucesso!";
-        } else {
-            echo "Erro ao inserir dados: " . $stmt->error;
-        }
-        
-
-        // $query_avatar = "INSERT INTO avatar(pele_Avat, rosto_Avat, cabelo_Avat, torso_Avat, pernas_Avat) VALUES (:pele, :rosto, :cabelo, :torso, :pernas)"
-        // or die("Error in the create Responsavel table...".$link->connect_error);
-        // $resul_avatar = $link->prepare($query_avatar);
-        // $resul_avatar->bind_param(":pele", $pele);
-        // $resul_avatar->bind_param(":rosto", $rosto);
-        // $resul_avatar->bind_param(":cabelo", $cabelo);
-        // $resul_avatar->bind_param(":torso", $torso);
-        // $resul_avatar->bind_param(":pernas", $pernas);
-        // $resul_avatar->execute();
-
-    }
-?>
     <script src="./assets/scripts/javascript/moldaAvatar.js"></script>
-    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+
+    
 </body>
 </html>
